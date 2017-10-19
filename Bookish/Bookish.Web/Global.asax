c78376205aa1414bb532534676b2d91af3bb2a35
ASpoonyBard3 <samsonf21@gmail.com>
2017-10-19 15:20:49 +0100

    Added empty asp.net class

---
+++ Bookish/Bookish.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bookish.Web.MvcApplication" Language="C#" %>
---
+++ Bookish/Bookish.Web2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bookish.Web2.Startup))]
+namespace Bookish.Web2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
